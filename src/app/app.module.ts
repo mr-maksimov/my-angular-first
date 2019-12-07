62f0dde23463e93ca7b573b2d071245371b83a6a
mr-maksimov <tinks2@ya.ru>
2019-12-07 16:11:03 +0400

    lesson 7 end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router/';
 
 import { AppComponent } from './app.component';
 import { CarComponent } from './components/car/car.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+
+const appRoutes: Routes = [
+    {path: '', component:CarComponent},
+    {path: 'contacts', component:ContactsComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarComponent
+    CarComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
